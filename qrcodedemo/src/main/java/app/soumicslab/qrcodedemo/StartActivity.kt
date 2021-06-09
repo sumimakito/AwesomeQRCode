4eddc763109bf47016326e6b2c881bfdc29d276f
fahimfarhan <fahim.farhan@outlook.com>
2021-06-10 00:23:41 +0600

    :zap: Add a demo module

---
+++ qrcodedemo/src/main/java/app/soumicslab/qrcodedemo/StartActivity.kt
@@ -0,0 +1,11 @@
+package app.soumicslab.qrcodedemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StartActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_start)
+  }
+}
